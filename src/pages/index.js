34f6fde33e0aec6d6abbfd5cbe9728b98de6e070
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-11-05 17:01:04 +0000

    Commt

---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import Navbar from '../components/Navbar'
+import Sidebar from '../components/Sidebar'
+
+const Home = () => {
+    // Toggle mobile sidebar
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggle = () => {
+        setIsOpen(!isOpen)
+    }
+
+    return (
+        <>
+          <Sidebar isOpen={isOpen} toggle={toggle} />
+          <Navbar toggle={toggle} />   
+        </>
+    )
+}
+
+export default Home
