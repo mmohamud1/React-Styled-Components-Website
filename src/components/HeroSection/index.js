5442b31459b155eff13f82a492b9c4bafc0342dc
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-11-06 11:42:58 +0000

    Commit

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { HeroContainer, HeroBg, VideoBg } from './HeroElements'
+import { HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1, HeroP, HeroBtnWrapper, ArrowForward, ArrowRight } from './HeroElements'
 import Video from '../../videos/video.mp4'
 
 const HeroSection = () => {
@@ -8,6 +8,13 @@ const HeroSection = () => {
             <HeroBg>
                 <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
             </HeroBg>
+            <HeroContent>
+                <HeroH1>Virtual Banking Made Easy</HeroH1>
+                <HeroP>Sign up for a new account and recieve £250 in credit towards your next payment</HeroP>
+                <HeroBtnWrapper>
+                    <Button to="signup">Get Started {hover ? <ArrowForward/> : <ArrowRight/>}</Button>
+                </HeroBtnWrapper>
+            </HeroContent>
         </HeroContainer>
     )
 }
