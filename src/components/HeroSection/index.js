b4ce8fd2fdb8d182eb13ae211c7cba98dc992b6e
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-11-06 11:51:36 +0000

    Commit

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1, HeroP, HeroBtnWrapper, ArrowForward, ArrowRight } from './HeroElements'
 import Video from '../../videos/video.mp4'
 
 const HeroSection = () => {
+    const [hover, setHover] = useState(false)
+
+    const onHover = () => {
+        setHover(!hover)
+    }
+
     return (
         <HeroContainer>
             <HeroBg>
@@ -12,7 +18,7 @@ const HeroSection = () => {
                 <HeroH1>Virtual Banking Made Easy</HeroH1>
                 <HeroP>Sign up for a new account and recieve £250 in credit towards your next payment</HeroP>
                 <HeroBtnWrapper>
-                    <Button to="signup">Get Started {hover ? <ArrowForward/> : <ArrowRight/>}</Button>
+                    <Button to="signup" onMouseEnter={onHover} onMouseLeave={onHover}>Get Started {hover ? <ArrowForward/> : <ArrowRight/>}</Button>
                 </HeroBtnWrapper>
             </HeroContent>
         </HeroContainer>
