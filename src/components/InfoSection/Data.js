ac0a667c4d685b3239d484566680414d58e8527d
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-11-06 14:24:50 +0000

    Commit

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -1,3 +1,11 @@
+import img1 from '../../images/svg1.svg';
+import img2 from '../../images/svg2.svg';
+import img3 from '../../images/svg3.svg';
+import img4 from '../../images/svg4.svg';
+import img5 from '../../images/svg7.svg';
+import img6 from '../../images/svg6.svg';
+
+
 export const homeObjOne = {
     id: 'about',
     lightBg: false,
@@ -8,7 +16,7 @@ export const homeObjOne = {
     description: 'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.',
     buttonLabel: 'Get Started',
     imgStart: false,
-    img: require('../../images/svg1.svg'),
+    img: img1,
     alt: 'car',
     dark: true,
     primary: true,
