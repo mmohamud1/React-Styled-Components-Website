3069dfe4fc2876de40e8e89151dea0125f5d8d06
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-11-07 12:24:10 +0000

    Commit

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,14 @@
 import React from 'react';
+import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter, FaYoutube } from 'react-icons/fa';
+import {
+  FooterContainer,
+  FooterWrap,
+  FooterLinksContainer,
+  FooterLinksWrapper,
+  FooterLinkItems,
+  FooterLinkTitle,
+  FooterLink,
+} from './FooterElements';
 
 const Footer = () => {
   return (
@@ -8,14 +18,61 @@ const Footer = () => {
           <FooterLinksWrapper>
             <FooterLinkItems>
               <FooterLinkTitle>About Us</FooterLinkTitle>
-              <FooterLink to='/signin'>How it works</FooterLink>
-              <FooterLink to='/signin'>Testimonials</FooterLink>
-              <FooterLink to='/signin'>Careers</FooterLink>
-              <FooterLink to='/signin'>Investors</FooterLink>
-              <FooterLink to='/signin'>Terms of service</FooterLink>
+              <FooterLink to='/'>How it works</FooterLink>
+              <FooterLink to='/'>Careers</FooterLink>
+              <FooterLink to='/'>Investors</FooterLink>
+              <FooterLink to='/'>Terms of service</FooterLink>
+            </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle>Videos</FooterLinkTitle>
+              <FooterLink to='/'>Submit Video</FooterLink>
+              <FooterLink to='/'>Ambassadors</FooterLink>
+              <FooterLink to='/'>Agency</FooterLink>
+              <FooterLink to='/'>Influencer</FooterLink>
+            </FooterLinkItems>
+          </FooterLinksWrapper>
+          <FooterLinksWrapper>
+            <FooterLinkItems>
+              <FooterLinkTitle>Contact Us</FooterLinkTitle>
+              <FooterLink to='/'>Contact</FooterLink>
+              <FooterLink to='/'>Support</FooterLink>
+              <FooterLink to='/'>Destinations</FooterLink>
+              <FooterLink to='/'>Sponsorships</FooterLink>
+            </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle>Our Appoach</FooterLinkTitle>
+              <FooterLink to='/'>Schemes</FooterLink>
+              <FooterLink to='/'>Donations</FooterLink>
+              <FooterLink to='/'>Partners</FooterLink>
+              <FooterLink to='/'>Insight</FooterLink>
             </FooterLinkItems>
           </FooterLinksWrapper>
         </FooterLinksContainer>
+        <SocialMedia>
+          <SocialMediaWrap>
+            <SocialLogo to='/'>moMoney</SocialLogo>
+            <WebsiteRights>
+              Created by Mohamed Mohamud &copy; 2020
+            </WebsiteRights>
+            <SocialIcons>
+              <SocialIconLink href='/' aria-label='Facebook'>
+                <FaFacebook />
+              </SocialIconLink>
+              <SocialIconLink href='/' aria-label='Instagram'>
+                <FaInstagram />
+              </SocialIconLink>
+              <SocialIconLink href='/' aria-label='Youtube'>
+                <FaYoutube />
+              </SocialIconLink>
+              <SocialIconLink href='/' aria-label='Twitter'>
+                <FaTwitter />
+              </SocialIconLink>
+              <SocialIconLink href='/' aria-label='Linkedin'>
+                <FaLinkedin />
+              </SocialIconLink>
+            </SocialIcons>
+          </SocialMediaWrap>
+        </SocialMedia>
       </FooterWrap>
     </FooterContainer>
   );
