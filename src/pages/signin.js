4e00931fb48f33bc7ee684c518dfead77047378f
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-11-07 14:18:43 +0000

    Commit

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Signin from '../components/Signin'
 
 const SigninPage = () => {
     return (
         <div>
-            <h1>Sign in</h1>
+            <Signin />
         </div>
     )
 }
