782f5ba45db1e5d778b3672c993726521a129129
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-11-07 11:51:08 +0000

    Commit

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/" component={Home} exact />
-        <Route path="/signin" component={SigninPage} exact />
+        <Route path='/' component={Home} exact />
+        <Route path='/signin' component={SigninPage} exact />
       </Switch>
     </Router>
   );
