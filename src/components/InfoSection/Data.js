d0e6b9b2319e89897f673a8d9fb7781d0d8d60ca
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-11-08 10:57:45 +0000

    Commit

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -7,7 +7,7 @@ export const homeObjOne = {
   lightBg: false,
   lightText: true,
   lightTextDesc: true,
-  topLine: 'Premium Bank',
+  topLine: 'Premium Platform',
   headline: 'Unlimited Transactions with zero fees',
   description:
     'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.',
